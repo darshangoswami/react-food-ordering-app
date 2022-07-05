3e51fb478753e29ab3b42b753be1124b65c726b3
darshangoswami <goswamidarshan999@gmail.com>
2022-07-05 22:21:59 +0530

    first commit

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,31 @@
+import { Fragment } from "react";
+import ReactDOM from "react-dom";
+import classes from "./Modal.module.css";
+
+const BackDrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onClose} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const protalElement = document.getElementById("overlays");
+
+const Modal = (props) => {
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(<BackDrop onClose={props.onClose} />, protalElement)}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        protalElement
+      )}
+    </Fragment>
+  );
+};
+
+export default Modal;
